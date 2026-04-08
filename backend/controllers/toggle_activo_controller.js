a1c72ee7ce4d5877dde27a5f0e69b1e523b27315
Jonnathan <stvns.j@proton.me>
2026-04-08 02:08:15 -0400

    toggle_activo_controller

---
+++ backend/controllers/toggle_activo_controller.js
@@ -0,0 +1,20 @@
+
+const {queryAsync} = require("../services/dbv2")
+
+
+exports.toggle_activo_controller = asyncHandler(async (req, res) => {
+
+    const empleado_id = req.params.empleadoid
+    const proyecto_id = req.params.proyectoid
+
+    const rows = await queryAsync(
+        "select toggle_activo(?,?) AS activo",
+        [empleado_id,proyecto_id]
+    );
+
+    const activo = rows[0].activo;
+
+
+  return res.status(200).json({ activo });
+
+});
